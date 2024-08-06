af972413511eacccf1f70a6b909d6a4890374d71
monukdjavadev <monukd01dev@gmail.com>
2024-08-06 21:38:44 +0530

    day-3 done

--- React/Day-3/app.js
+++ React/Day-3/app.js
@@ -8,6 +8,68 @@ const root = reactDOM.createRoot(document.getElementById("root"));
 //   { id: "heading" },
 //   "This is the basic and working fine"
 // );
-const heading = <h1 className="heading">This is from JSX 🚀 </h1>;
 
-root.render(heading);
+// jsx is converted to core-react by bable and the core-react code return an object and that object is rendered into the root using the render() method of react-dom
+
+//element
+const heading = <h1 className="heading">This is from JSX 🚀 Cool</h1>;
+const robbrey = (
+  <section>
+    <h2>Why robbers?</h2>
+    <p>
+      There could be something but the lorem not working so i have to write
+      something that that's why i am writing rubbish hr athe there wourld we no
+      i ama jsut out of workd what can i sya
+    </p>
+  </section>
+);
+
+//functional component
+// styled component
+const Card = function () {
+  const cardStyle = {
+    border: "1px solid black",
+    width: "200px",
+    borderRadius: "10px",
+    overflow: "hidden",
+    padding: "10px",
+  };
+  return (
+    <div className="card" style={cardStyle}>
+      <img
+        src={`https://picsum.photos/id/${Math.floor(
+          Math.random() * 100
+        )}/200/200`}
+      />
+      <h2>Learning is important</h2>
+      <p>
+        In US most of the clg students do drugs why and how I knew that even I
+        don't know cause lorem isn't working here.
+      </p>
+      {robbrey}
+    </div>
+  );
+};
+
+// styled component
+
+const CardContainer = () => {
+  const CardContainerStyle = {
+    display: "flex",
+    justifyContent: "space-evenly",
+    gap: " 20px",
+    flexWrap: "wrap",
+    alignItems: "center",
+  };
+
+  return (
+    <div className="card-container" style={CardContainerStyle}>
+      <Card />
+      <Card></Card>
+      {Card()}
+      {[<Card />, <Card />, <Card />, <Card />, <Card />, <Card />]}
+    </div>
+  );
+};
+
+root.render(<CardContainer />);
